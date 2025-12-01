21014f388dbaf4d7bcb181e095e5d185fa5d5575
Rohan Bangera <162192148+superrohan@users.noreply.github.com>
2025-12-01 18:45:16 +0530

    Add majority element solutions using two algorithms
    Implemented two methods to find the majority element in an array: Boyer-Moore Voting Algorithm and sorting approach.

---
+++ 2. Majority Elements.java
@@ -0,0 +1,67 @@
+/*
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+*/
+
+//Boyer-Moore Voting Algorithm
+class Solution {
+    public int majorityElement(int[] nums) {
+        int candidate = nums[0];
+        int count = 1;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+                count = 1;
+            }
+            else if(num == candidate) {
+                count++;
+            }
+            else {
+                count--;
+            }
+        }
+        return candidate;
+    }
+}
+
+/*
+It maintains:
+
+candidate: current guess for the majority
+count: confidence in the candidate
+As you scan:
+
+If count == 0, adopt the current number as the new candidate.
+If the current number equals candidate, increment count; otherwise, decrement it.
+If a true majority exists, this process guarantees the final candidate is that majority.
+*/
+
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Sorting the Array
+
+/*
+If we sort the array, all identical elements naturally group together. Since the majority element appears more than n/2 times, it must occupy the entire middle region of the sorted array.
+That means the element at index n/2 will always be the majority element.
+*/
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
+    }
+}
